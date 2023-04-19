c796a920a0a6b90ecc5d62ce3aff03fcea82085f
Marcos Crisostomo <marcos.cr.mtz@gmail.com>
2023-04-18 22:38:20 -0600

    Init project

---
+++ mcsv-usuarios/src/main/java/org/mmcm/springcloud/mcsv/usuarios/McsvUsuariosApplication.java
@@ -0,0 +1,13 @@
+package org.mmcm.springcloud.mcsv.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvUsuariosApplication.class, args);
+	}
+
+}
