ac51dcf64df61e91151874d596962b63fb5732bd
Marcos Crisostomo <marcos.cr.mtz@gmail.com>
2023-04-20 12:13:23 -0600

    MCSV -  VALIDACIONES & Querys Personalizados

--- mcsv-usuarios/src/main/java/org/mmcm/springcloud/mcsv/usuarios/controllers/UsuarioController.java
+++ mcsv-usuarios/src/main/java/org/mmcm/springcloud/mcsv/usuarios/controllers/UsuarioController.java
@@ -5,10 +5,12 @@ import org.mmcm.springcloud.mcsv.usuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import javax.validation.Valid;
+import java.util.*;
+
 
 @RestController
 public class UsuarioController {
@@ -31,15 +33,38 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public ResponseEntity<?> crear(@RequestBody Usuario usuario) {
+    public ResponseEntity<?> crear(@Valid @RequestBody Usuario usuario, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return validar(result);
+        }
+
+        if (!usuario.getEmail().isEmpty() && service.existePorEmail(usuario.getEmail())) {
+            return ResponseEntity.badRequest()
+                    .body(Collections
+                            .singletonMap("mensaje", "Ya existe un usuario con ese correo electronico!"));
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> editar(@RequestBody Usuario usuario, @PathVariable Long id) {
+    public ResponseEntity<?> editar(@Valid @RequestBody Usuario usuario, BindingResult result, @PathVariable Long id) {
+
+        if (result.hasErrors()) {
+            return validar(result);
+        }
+
         Optional<Usuario> o = service.porId(id);
         if (o.isPresent()) {
             Usuario usuarioDb = o.get();
+            if (!usuario.getEmail().isEmpty() &&
+                    !usuario.getEmail().equalsIgnoreCase(usuarioDb.getEmail()) &&
+                    service.porEmail(usuario.getEmail()).isPresent()) {
+                return ResponseEntity.badRequest()
+                        .body(Collections
+                                .singletonMap("mensaje", "Ya existe un usuario con ese correo electronico!"));
+            }
+
             usuarioDb.setNombre(usuario.getNombre());
             usuarioDb.setEmail(usuario.getEmail());
             usuarioDb.setPassword(usuario.getPassword());
@@ -57,4 +82,13 @@ public class UsuarioController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    private ResponseEntity<Map<String, String>> validar(BindingResult result) {
+        Map<String, String> errores = new HashMap<>();
+        result.getFieldErrors().forEach(err -> {
+            errores.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errores);
+    }
 }
+
--- mcsv-usuarios/src/main/java/org/mmcm/springcloud/mcsv/usuarios/repositories/UsuarioRepository.java
+++ mcsv-usuarios/src/main/java/org/mmcm/springcloud/mcsv/usuarios/repositories/UsuarioRepository.java
@@ -1,7 +1,17 @@
 package org.mmcm.springcloud.mcsv.usuarios.repositories;
 
 import org.mmcm.springcloud.mcsv.usuarios.models.entity.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+
+    @Query("select u from Usuario u where u.email=?1")
+    Optional<Usuario> porEmail(String email);
+
+    boolean existsByEmail(String email);
+
 }
